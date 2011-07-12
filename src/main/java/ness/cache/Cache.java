de46e7e549b97ff2473635afdf599b7c954f0cfc
Steven Schlansker <steven@nesscomputing.com>
2011-07-12 15:50:23 -0700

    Document the Cache class.

--- src/main/java/ness/cache/Cache.java
+++ src/main/java/ness/cache/Cache.java
@@ -6,6 +6,11 @@ import java.util.Map;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
+/**
+ * Provides a provider-neutral caching layer which has asynchronous writes and synchronous
+ * reads.  The API does not promise any sorts of consistency, although the provider may
+ * be configured (via {@link CacheConfiguration}) to provide various guarantees.
+ */
 @Singleton
 public class Cache implements InternalCacheProvider {
     private final InternalCacheProvider provider;
@@ -17,6 +22,17 @@ public class Cache implements InternalCacheProvider {
         this.cacheStatistics = cacheStatistics;
     }
     
+    /**
+     * Provide a view of this cache which automatically has the given namespace filled in.  Intended to be used
+     * in constructors, e.g.
+     * <pre>
+     * private final NamespacedCache myCache;
+     * @Inject
+     * MyCoolClass(Cache cache) {
+     *     myCache = cache.withNamespace("extraCoolWithCheese");
+     * }
+     * </pre>
+     */
     public NamespacedCache withNamespace(String namespace) {
         return new NamespacedCache(this, namespace);
     }
