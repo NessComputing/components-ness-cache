1086d12afea5ef22855e17ee0022b8fdff31b3a5
Steven Schlansker <steven@nesscomputing.com>
2011-07-13 21:39:46 -0700

    Add a CacheModule c'tor that takes a Config object.

--- src/main/java/ness/cache/CacheModule.java
+++ src/main/java/ness/cache/CacheModule.java
@@ -1,23 +1,29 @@
 package ness.cache;
 
+import io.trumpet.config.Config;
+
 import java.lang.annotation.Annotation;
 
 import com.google.inject.PrivateModule;
 
 public class CacheModule extends PrivateModule {
-    
+
     private final Annotation bindingAnnotation;
     private final CacheConfiguration config;
+
+    public CacheModule(Config config) {
+        this (config.getBean(CacheConfiguration.class));
+    }
 
     public CacheModule(CacheConfiguration config) {
         this (config, null);
     }
-    
+
     public CacheModule(CacheConfiguration config, Annotation bindingAnnotation) {
         this.config = config;
         this.bindingAnnotation = bindingAnnotation;
     }
-    
+
     @Override
     protected void configure() {
         if (bindingAnnotation != null) {
@@ -27,34 +33,34 @@ public class CacheModule extends PrivateModule {
             bind (Cache.class);
             expose (Cache.class);
         }
-        
+
         bind (CacheStatisticsManager.class);
-        
+
         bind (CacheConfiguration.class).toInstance(config);
-        
+
         // Internal bindings are not exposed, so do not need to be annotated.
-        
+
         switch (config.getCacheType()) {
         case NONE:
         case none:
             bind (InternalCacheProvider.class).to(NullProvider.class);
             break;
-            
+
         case JVM:
         case jvm:
             bind (InternalCacheProvider.class).to(JvmCacheProvider.class);
             break;
-            
+
         case MEMCACHE:
         case memcache:
             bind (InternalCacheProvider.class).to(MemcacheProvider.class);
             bind (MemcachedClientFactory.class);
             bind (CacheTopologyProvider.class);
             break;
-            
+
         default:
             throw new IllegalStateException("Unrecognized cache type " + config.getCacheType());
         }
-        
+
     }
 }
