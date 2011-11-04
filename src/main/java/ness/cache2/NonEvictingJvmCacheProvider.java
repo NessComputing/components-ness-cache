0491aefe22b4f6e890d7af28cb71a0d4986c0af0
Henning Schmiedehausen <henning@likeness.com>
2011-11-03 17:05:41 -0700

    remove tc-base from the dependencies

--- src/main/java/ness/cache2/NonEvictingJvmCacheProvider.java
+++ src/main/java/ness/cache2/NonEvictingJvmCacheProvider.java
@@ -1,7 +1,6 @@
 package ness.cache2;
 
 import io.trumpet.log.Log;
-import io.trumpet.util.Pair;
 
 import java.util.Collection;
 import java.util.Map;
@@ -10,22 +9,22 @@ import com.google.common.collect.Maps;
 import com.google.inject.Singleton;
 
 /** An in JVM cache that does not evict its keys due to memory pressure.
- * 
+ *
  * Note: once ehcache 2.5 is released this can be implemented as a subclass of JvmCacheProvider with pinning in ehcache.
- * 
+ *
  * @author christopher
  *
  */
 @Singleton
 public class NonEvictingJvmCacheProvider implements InternalCacheProvider {
 	private final static Log LOG = Log.findLog();
-	private final Map<Pair<String, String>, byte[]> map = Maps.newConcurrentMap();
+	private final Map<Map.Entry<String, String>, byte[]> map = Maps.newConcurrentMap();
 
 	@Override
 	public void set(String namespace, Map<String, CacheStore> stores) {
 		for (Map.Entry<String, CacheStore> entry: stores.entrySet()) {
 			LOG.trace("%s setting %s:%s", this, namespace, entry.getKey());
-			map.put(Pair.of(namespace, entry.getKey()), entry.getValue().getBytes());
+			map.put(Maps.immutableEntry(namespace, entry.getKey()), entry.getValue().getBytes());
 		}
 	}
 
@@ -33,7 +32,7 @@ public class NonEvictingJvmCacheProvider implements InternalCacheProvider {
 	public Map<String, byte[]> get(String namespace, Collection<String> keys) {
 		Map<String, byte[]> ret = Maps.newHashMap();
 		for (String key: keys) {
-			byte[] data = map.get(Pair.of(namespace, key));
+			byte[] data = map.get(Maps.immutableEntry(namespace, key));
 			LOG.trace("%s getting %s:%s=%s", this, namespace, key, data);
 			ret.put(key, data);
 		}
@@ -44,8 +43,7 @@ public class NonEvictingJvmCacheProvider implements InternalCacheProvider {
 	public void clear(String namespace, Collection<String> keys) {
 		for (String key: keys) {
 			LOG.trace("%s clearing %s:%s", this, namespace, key);
-			map.remove(Pair.of(namespace, key));
+			map.remove(Maps.immutableEntry(namespace, key));
 		}
 	}
-
 }
