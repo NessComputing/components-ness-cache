c67f8f21c290bf5dc6f39310a5bd4db882b3788d
Henning Schmiedehausen <henning@likeness.com>
2012-01-22 19:06:57 -0800

    JMX export memcache nodes.

--- src/main/java/ness/cache2/NessMemcachedConnectionFactory.java
+++ src/main/java/ness/cache2/NessMemcachedConnectionFactory.java
@@ -1,9 +1,16 @@
 package ness.cache2;
 
+import io.trumpet.log.Log;
+
 import java.net.SocketAddress;
 import java.nio.channels.SocketChannel;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+
+import javax.annotation.Nullable;
+
+import org.weakref.jmx.JmxException;
+import org.weakref.jmx.MBeanExporter;
 
 import net.spy.memcached.KetamaConnectionFactory;
 import net.spy.memcached.MemcachedNode;
@@ -14,17 +21,30 @@ import net.spy.memcached.protocol.binary.BinaryOperationFactory;
 import net.spy.memcached.transcoders.Transcoder;
 
 import com.google.inject.Inject;
+import com.google.inject.name.Named;
 
 /**
  * A ConnectionFactory which is Ketama and Binary capable, and uses the custom Ness transcoder.
  */
 public class NessMemcachedConnectionFactory extends KetamaConnectionFactory {
 
+    private static final Log LOG = Log.findLog();
+
     private final CacheConfiguration configuration;
+    private final String cacheName;
+    private MBeanExporter exporter = null;
 
     @Inject
-    NessMemcachedConnectionFactory(CacheConfiguration configuration) {
+    NessMemcachedConnectionFactory(final CacheConfiguration configuration,
+                                   @Nullable @Named("cacheName") final String cacheName) {
         this.configuration = configuration;
+        this.cacheName = cacheName == null ? "[default]" : cacheName;
+    }
+
+    @Inject(optional=true)
+    void injectMBeanExporter(final MBeanExporter exporter)
+    {
+        this.exporter = exporter;
     }
 
     // Use our custom transcoder
@@ -41,13 +61,25 @@ public class NessMemcachedConnectionFactory extends KetamaConnectionFactory {
     public MemcachedNode createMemcachedNode(SocketAddress sa,
             SocketChannel c, int bufSize) {
         boolean doAuth = false;
-        return new BinaryMemcachedNodeImpl(sa, c, bufSize,
+        final MemcachedNode node = new BinaryMemcachedNodeImpl(sa, c, bufSize,
             new LinkedBlockingQueue<Operation>(configuration.getReadQueueSize()),
             new LinkedBlockingQueue<Operation>(configuration.getWriteQueueSize()),
             new ArrayBlockingQueue<Operation>(configuration.getIncomingQueueSize()),
             configuration.getOperationQueueBlockTime().getMillis(),
             doAuth,
             getOperationTimeout());
+
+        if (exporter != null) {
+            final String name = String.format("ness.memcached:cache=%s,node=%s", cacheName, sa.toString());
+            try {
+                exporter.export(name, node);
+            }
+            catch (JmxException je) {
+                LOG.warnDebug(je, "Could not export new memcached node for %s", sa);
+            }
+        }
+
+        return node;
     }
 
     @Override
